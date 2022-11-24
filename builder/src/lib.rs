946d228dccc2760e828ce96f3fe1e6abb7546861
Koki Kubo <c11h17n6o8@gmail.com>
2022-11-24 19:08:00 +0900

    03-call-setters

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,20 +1,53 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, Data};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let struct_name = input.ident;
     let builder_name = format_ident!("{}Builder", struct_name);
+    let fields = match input.data {
+        Data::Struct(st) => {
+            st.fields
+        },
+        _ => unimplemented!()
+    };
+
+    let mut builder_fields = Vec::new();
+    let mut fields_inits = Vec::new();
+    let mut setters = Vec::new();
+    for field in fields {
+        let ident = field.ident;
+        let ty = field.ty;
+
+        builder_fields.push(quote!{
+            #ident: Option<#ty>,
+        });
+        fields_inits.push(quote!{
+            #ident: None,
+        });
+        setters.push(quote! {
+            pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                self.#ident = Some(#ident);
+                self
+            }
+        });
+    }
 
     let expanded = quote! {
         pub struct #builder_name {
+            #( #builder_fields )*
         }
 
-        impl Command {
+        impl #builder_name {
+            #( #setters )*
+        }
+
+        impl #struct_name {
             pub fn builder() -> #builder_name {
                 #builder_name {
+                    #( #fields_inits )*
                 }
             }
         }
