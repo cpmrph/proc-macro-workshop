c7003bd76ca977da1a87b6ab0ecd7ee2476d3f41
Koki Kubo <c11h17n6o8@gmail.com>
2022-11-24 16:22:31 +0900

    02-create-builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,13 +1,23 @@
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
+    let struct_name = input.ident;
+    let builder_name = format_ident!("{}Builder", struct_name);
 
     let expanded = quote! {
+        pub struct #builder_name {
+        }
 
+        impl Command {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                }
+            }
+        }
     };
 
     TokenStream::from(expanded)
